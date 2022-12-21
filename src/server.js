0ae9bf61cc80960a38a4b576483c5d8ab318ad24
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-12-21 11:57:37 -0300

    feat: imported dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-
 import express from "express";
 import cors from "cors";
 import router from "./routes/router.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(router);
